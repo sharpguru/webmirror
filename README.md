a89091f84a1d7f301e2e3ee06037ef9faa27bba2
Brian Foote <brian@gurubyte.com>
2017-06-11 22:03:43 -0700

    Completed basic messaging

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 ﻿# webmirror
+Hosts communications server for chat and file services over socket.io
 
+## Features:
+- chat
+- file services
 
+## Usage
+Starting server:
+~~~
+Command Line: npm start <port>
+~~~
+
+## Client connectivity
+Clients connect using socket.io to the hosted port. Checkout webchat for
+an example Client
+
+## Command list
+Prefix all commands with "."
+- .help: Display command list
+- .hello: Returns system greeting
+- .name [name]: Get or set your name
+- .users: List users online
+- .version: Returns system greeting
+- .whoami: Returns who you are
+
+# System greeting
+- displays date and time, version and current channel
--- webmirror.js
+++ webmirror.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var logger = require('morgan');
 var low = require('lowdb');
+var _ = require('lodash');
 
 app.use(logger('dev'));
 
@@ -16,23 +17,48 @@ app.get('/', function (req, res) {
 var server = app.listen(port);
 var io = require('socket.io')(server);
 
+// Server info
+var ServerInfo = "webmirror Express server v1.0";
+
 server.listen(port, function () {
-    console.log('webmirror Express server listening on port ' + port);
+    ServerInfo = ServerInfo + ' listening on port ' + port;
+    console.log(ServerInfo);
 });
+
+
 
 // database
 const db = low();
-db.defaults({ messages: [] }).write(); // create empty array of messages
+db.defaults({ messages: [], users: [], help: [] }).write(); // init stuff
+
+// Set help messages collection
+function addHelp(msg) {
+  db.get('help').push({ msg: msg }).write();
+}
+
+addHelp('Prefix all commands with "."');
+addHelp('- .help: Display command list');
+addHelp('- .hello: Returns system greeting');
+addHelp('- .name: Get or set your name');
+addHelp('- .users: List users online');
+addHelp('- .version: Returns system greeting');
+addHelp('- .whoami: Returns who you are');
+var helpmessages = db.get('help').value();
+
+console.log(helpmessages);
+
+
 const uuid = require('uuid');
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    var id = socket.id;
+    console.log('User connected: ' + socket.id);
     var collection = db.get('messages').takeRight(10).value();
     console.log(collection);
     //console.log('db state:');
     //console.log(db.getState());
 
-    // sending old messages 
+    // sending old messages
     collection.forEach(chat => {
         socket.emit('chat', chat.msg);
     });
@@ -51,11 +77,84 @@ io.on('connection', function (socket) {
         var dbstate = db.getState();
         dbstate.messages = msgs;
         db.setState(dbstate);
-        
+
         // console.log(db.getState()); // log all messages
         console.log(msg); // log current message
-
     });
 
-    socket.emit('connected', 'You are now connected to webmirror');
+    socket.on('help', function() {
+      console.log('user needs help: ' + id);
+
+      // sending help
+      helpmessages.forEach(hlp => {
+          socket.emit('returnmessage', hlp.msg);
+      });
+    });
+
+    socket.on('hello', function () {
+      console.log('say hello to user: ' + id);
+
+      socket.emit('returnmessage', 'You are now connected to '
+        + ServerInfo + '. '
+        + 'System date and time: '
+        + Date()
+      );
+
+      //var currentRoom = socket.rooms[Object.keys(socket.rooms)];
+      //socket.emit('returnmessage', 'You are in room: ' + currentRoom);
+    })
+
+    socket.on('name', function (newname) {
+
+      console.log('newname: ' + newname);
+      if (!newname) {
+        var usr = getCurrentUser(id).value();
+        var usrname = (usr && usr.name) ? usr.name : '(unknown)';
+        console.log('returning user name...' + usrname);
+        socket.emit('returnmessage', usrname);
+      } else {
+
+        console.log('setting username: ' + newname);
+        console.log(getCurrentUser(id).value());
+
+        getCurrentUser(id)
+          .assign({name: newname})
+          .write();
+
+        socket.emit('returnmessage', 'username set: ' + newname);
+      }
+    });
+
+    socket.on('users', function() {
+      var users = db.get('users').value();
+      console.log(users);
+      socket.emit('returnmessage', users);
+    })
+
+    socket.on('version', function() {
+      socket.emit('returnmessage', ServerInfo);
+    })
+
+    socket.on('whoami', function(whom) {
+      console.log('who ' + whom);
+      console.log(id);
+
+      socket.emit('returnmessage', 'You are ' + getCurrentUser(id).value().name);
+    });
+
+    // Get current user
+    function getCurrentUser(id) {
+      console.log('Looking for user: ' + id);
+      var user = db.get('users').find({ 'socket': id });
+      console.log(user.value());
+      return user;
+    }
+
+    // Save current user
+    var user = {
+      socket: id
+    }
+
+    db.get('users').push(user).write();
+    socket.emit('connected', 'You are now connected to ' + ServerInfo);
 });
